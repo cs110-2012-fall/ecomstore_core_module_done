d224fe7d9b85d29e59db9bdac1ab0314f9654db7
dotcom900825 <dotcom900825@gmail.com>
2012-11-21 14:30:21 +0800

    fixed missing shopping cart and user module

--- ecomstore/preview/views.py
+++ ecomstore/preview/views.py
@@ -1,20 +1,20 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-
+from django.template import RequestContext
 #def home(request):
 #	return render_to_response("index_test.html")
 
 def home(request):
-	return render_to_response("index.html")
+	return render_to_response("index.html",locals(),context_instance=RequestContext(request))
 
 def contact(request):
-	return render_to_response("contact.html")
+	return render_to_response("contact.html",locals(),context_instance=RequestContext(request))
 
 def about(request):
-	return render_to_response("about.html")
+	return render_to_response("about.html",locals(),context_instance=RequestContext(request))
 
 def services(request):
-	return render_to_response("services.html")
+	return render_to_response("services.html",locals(),context_instance=RequestContext(request))
 
 def shop_online(request):
-	return render_to_response("shop_online.html")
+	return render_to_response("shop_online.html",locals(),context_instance=RequestContext(request))
